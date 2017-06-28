153815c25dedde7596ec220d2001ba6f202bccac
Ellen T Arena <arenaellen@gmail.com>
2017-06-28 14:44:50 -0500

    Accumulator: convert TwinCursor to IterablePair
    All constructors now take IterablePairs.

--- src/main/java/net/imagej/ops/coloc/Accumulator.java
+++ src/main/java/net/imagej/ops/coloc/Accumulator.java
@@ -22,49 +22,51 @@
 package net.imagej.ops.coloc;
 
 import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Pair;
 
 /**
- * A class allowing an easy accumulation of values visited by a
- * TwinCursor. After instantiation the sum of channel one,
+ * A class allowing an easy accumulation of values visited by an
+ * IterablePair. After instantiation the sum of channel one,
  * channel two, products with them self and a product of both of
  * them will be available. It additionally provides the possibility
- * to subtract values from the data before the adding them to the
- * sum.
+ * to subtract values from the data before adding them to the sum.:
  * 
- * @author Johannes Schindelin and Tom Kazimiers
+ * @author Johannes Schindelin
+ * @author Tom Kazimiers
+ * @author Ellen T Arena
  */
-public abstract class Accumulator<T extends RealType< T >> {
+public abstract class Accumulator<T extends RealType< T >, U extends RealType<U>> {
 	protected double x, y, xx, xy, yy;
 	protected int count;
 
 	/**
-	 * The two values x and y from each cursor iteration to get
+	 * The two values x and y from each iteration to get
 	 * summed up as single values and their combinations.
 	 */
-	public Accumulator(final TwinCursor<T> cursor) {
-		this(cursor, false, 0.0d, 0.0d);
+	public Accumulator(final Iterable<Pair<T, U>> samples) {
+		this(samples, false, 0.0d, 0.0d);
 	}
 
 	/**
-	 * The two values (x - xDiff) and (y - yDiff) from each cursor
+	 * The two values (x - xDiff) and (y - yDiff) from each
 	 * iteration to get summed up as single values and their combinations.
 	 */
-	public Accumulator(final TwinCursor<T> cursor, double xDiff, double yDiff) {
-		this(cursor, true, xDiff, yDiff);
+	public Accumulator(final Iterable<Pair<T,U>> samples, double xDiff, double yDiff) {
+		this(samples, true, xDiff, yDiff);
 	}
 
-	protected Accumulator(final TwinCursor<T> cursor, boolean substract, double xDiff, double yDiff) {
-		while (cursor.hasNext()) {
-			cursor.fwd();
-
-			T type1 = cursor.getFirst();
-			T type2 = cursor.getSecond();
-
+	protected Accumulator(final Iterable<Pair<T, U>> samples, boolean substract, double xDiff, double yDiff) {
+		
+		for (Pair<T, U> sample : samples) {
+			
+			T type1 = sample.getA();
+			U type2 = sample.getB();
+			
 			if (!accept(type1, type2))
 				continue;
-
-			double value1 = type1.getRealDouble();
-			double value2 = type2.getRealDouble();
+			
+			double value1 = sample.getA().getRealDouble();
+			double value2 = sample.getB().getRealDouble();
 
 			if (substract) {
 				value1 -= xDiff;
@@ -80,7 +82,7 @@ public abstract class Accumulator<T extends RealType< T >> {
 		}
 	}
 
-	public abstract boolean accept(T type1, T type2);
+	public abstract boolean accept(T type1, U type2);
 
 	public double getX() {
 		return x;
