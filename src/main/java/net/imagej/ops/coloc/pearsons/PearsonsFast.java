6c726e6374d548b6bfcc8cebc7e9ad1fe1396cd9
Ellen T Arena <arenaellen@gmail.com>
2017-07-06 14:05:02 -0500

    Add Pearsons Op
    This pearsons op uses the Fast implementation version in Coloc 2.  There
    is also a helper PearsonsResult class to hold 3 outputs:
      correlationValue, correlationValueBelowThr, correlationValueAboveThrH.

--- src/main/java/net/imagej/ops/coloc/pearsons/PearsonsFast.java
+++ src/main/java/net/imagej/ops/coloc/pearsons/PearsonsFast.java
@@ -9,7 +9,6 @@ import net.imglib2.type.numeric.RealType;
 import net.imglib2.util.IterablePair;
 import net.imglib2.util.Pair;
 
-import org.scijava.ItemIO;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
@@ -17,62 +16,52 @@ import org.scijava.plugin.Plugin;
  * A class that represents the mean calculation of the two source images in the
  * data container. It implements the FAST calculation for Pearson's Correlation.
  *
- * @param <T>
+ * @author Ellen T Arena
  */
 @Plugin(type = Ops.Coloc.Pearsons.class)
 public class PearsonsFast<T extends RealType<T>, U extends RealType<U>> extends
-	AbstractBinaryFunctionOp<Iterable<T>, Iterable<U>, Double[]> implements
+	AbstractBinaryFunctionOp<Iterable<T>, Iterable<U>, PearsonsResult> implements
 	Ops.Coloc.Pearsons
 {
 
-	@Parameter(type = ItemIO.OUTPUT)
-	private Double pearsonsFastCorrelationValue;
+	@Parameter(required = false)
+	private T threshold1;
 
-	@Parameter(type = ItemIO.OUTPUT)
-	private Double pearsonsFastCorrelationValueBelowThr;
-
-	@Parameter(type = ItemIO.OUTPUT)
-	private Double pearsonsFastCorrelationValueAboveThr;
+	@Parameter(required = false)
+	private U threshold2;
 
 	@Override
-	public Double[] calculate(final Iterable<T> image1,
+	public PearsonsResult calculate(final Iterable<T> image1,
 		final Iterable<U> image2)
 	{
 		// get the 2 images for the calculation of Pearson's
 		final Iterable<Pair<T, U>> samples = new IterablePair<>(image1, image2);
 
-		// get the thresholds of the images
-		final T threshold1 = image1.getCh1MaxThreshold();
-		final U threshold2 = image2.getCh2MaxThreshold();
+		final PearsonsResult result = new PearsonsResult();
 
-		pearsonsFastCorrelationValue = fastPearsons(samples);
+		result.correlationValue = fastPearsons(samples, ThresholdMode.None);
+		
+		if (threshold1 != null && threshold2 != null) {
 
-		pearsonsFastCorrelationValueBelowThr = fastPearsons(samples, threshold1,
-			threshold2, ThresholdMode.Below);
+			result.correlationValueBelowThr = fastPearsons(samples,
+				ThresholdMode.Below);
 
-		pearsonsFastCorrelationValueAboveThr = fastPearsons(samples, threshold1,
-			threshold2, ThresholdMode.Above);
+			result.correlationValueAboveThr = fastPearsons(samples,
+				ThresholdMode.Above);
+		}
+
+		return result;
 	}
-
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	/**
 	 * Calculates Person's R value by using a fast implementation of the
-	 * algorithm. 
+	 * algorithm.
 	 *
-	 * @param <T> The image base type
 	 * @return Person's R value
-	 * @throws Exception 
+	 * @throws IllegalArgumentException If input data is statistically unsound.
 	 */
-	private <T extends RealType<T>> double fastPearsons(
-		final Iterable<Pair<T, U>> samples) throws Exception
-	{
-		return fastPearsons(samples, null, null, ThresholdMode.None);
-	}
-
-	private <T extends RealType<T>> double fastPearsons(
-		final Iterable<Pair<T, U>> samples, final T threshold1,
-		final U threshold2, final ThresholdMode tMode) throws Exception
+	private double fastPearsons(final Iterable<Pair<T, U>> samples,
+		final ThresholdMode tMode)
 	{
 		// the actual accumulation of the image values is done in a separate object
 		Accumulator<T, U> acc;
@@ -91,7 +80,8 @@ public class PearsonsFast<T extends RealType<T>, U extends RealType<U>> extends
 
 				@Override
 				final public boolean accept(final T type1, final U type2) {
-					return type1.compareTo(threshold1) < 0 || type2.compareTo(threshold2) < 0;
+					return type1.compareTo(threshold1) < 0 || type2.compareTo(
+						threshold2) < 0;
 				}
 			};
 		}
@@ -100,7 +90,8 @@ public class PearsonsFast<T extends RealType<T>, U extends RealType<U>> extends
 
 				@Override
 				final public boolean accept(final T type1, final U type2) {
-					return type1.compareTo(threshold1) > 0 || type2.compareTo(threshold2) > 0;
+					return type1.compareTo(threshold1) > 0 || type2.compareTo(
+						threshold2) > 0;
 				}
 			};
 		}
@@ -112,7 +103,7 @@ public class PearsonsFast<T extends RealType<T>, U extends RealType<U>> extends
 		final int count = acc.getCount();
 		final double invCount = 1.0 / count;
 
-		final double pearsons1 = acc.getXY() - (acc.getX() * acc.getX() * invCount);
+		final double pearsons1 = acc.getXY() - (acc.getX() * acc.getY() * invCount);
 		final double pearsons2 = acc.getXX() - (acc.getX() * acc.getX() * invCount);
 		final double pearsons3 = acc.getYY() - (acc.getY() * acc.getY() * invCount);
 		final double pearsonsR = pearsons1 / (Math.sqrt(pearsons2 * pearsons3));
@@ -127,11 +118,9 @@ public class PearsonsFast<T extends RealType<T>, U extends RealType<U>> extends
 	 * for fast and classic implementation.
 	 *
 	 * @param value The value to check.
-	 * @throws Exception
+	 * @throws IllegalArgumentException
 	 */
-	private static void checkForSanity(final double value, final int iterations)
-		throws Exception
-	{
+	private static void checkForSanity(final double value, final int iterations) {
 		if (Double.isNaN(value) || Double.isInfinite(value)) {
 			/* For the _fast_ implementation this could happen:
 			 *   Infinity could happen if only the numerator is 0, i.e.:
@@ -147,7 +136,7 @@ public class PearsonsFast<T extends RealType<T>, U extends RealType<U>> extends
 			 *   Additionally, if is zero for both channels at once you
 			 *   could get NaN. NaN
 			 */
-			throw new Exception(
+			throw new IllegalArgumentException(
 				"A numerical problem occured: the input data is unsuitable for this algorithm. Possibly too few pixels (in range were: " +
 					iterations + ").");
 		}
---
+++ src/main/java/net/imagej/ops/coloc/pearsons/PearsonsResult.java
@@ -0,0 +1,9 @@
+package net.imagej.ops.coloc.pearsons;
+
+public class PearsonsResult {
+	
+	public Double correlationValue;
+	public Double correlationValueBelowThr;
+	public Double correlationValueAboveThr;
+
+}
