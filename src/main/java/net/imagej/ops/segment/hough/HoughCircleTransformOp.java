068e7fe38a543a992913bb22770559d2842142fe
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2017-10-30 12:57:50 +0100

    Change mother interface for Hough transform marker interface.

--- src/main/java/net/imagej/ops/segment/hough/HoughCircleTransformOp.java
+++ src/main/java/net/imagej/ops/segment/hough/HoughCircleTransformOp.java
@@ -1,13 +1,17 @@
 package net.imagej.ops.segment.hough;
 
-import net.imagej.ops.Op;
+import net.imagej.ops.special.hybrid.UnaryHybridCF;
+import net.imglib2.IterableInterval;
+import net.imglib2.img.Img;
+import net.imglib2.type.BooleanType;
+import net.imglib2.type.numeric.real.DoubleType;
 
 /**
  * Marker interface for Hough circle transform ops.
  * 
  * @author Jean-Yves Tinevez
  */
-public interface HoughCircleTransformOp extends Op
+public interface HoughCircleTransformOp< T extends BooleanType< T > > extends UnaryHybridCF< IterableInterval< T >, Img< DoubleType > >
 {
 
 }
